0fb19f6cd5e9697fe8b36fc667b6f3b5619c7ce5
px <496869551@qq.com>
2020-03-17 17:25:54 +0800

    第一次提交

--- src/org/java/demo/Demo2.java
+++ src/org/java/demo/Demo2.java
@@ -3,5 +3,6 @@ package org.java.demo;
 public class Demo2 {
     public static void main(String[] args) {
         System.out.println("22222222222222222222222");
+        System.out.println("333333333333333333333333");
     }
 }
